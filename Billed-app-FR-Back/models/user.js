31ceafc1ee48fb6b2ac43cbfebf0d4270a07c855
queva <queval.loic@gmail.com>
2022-11-12 16:35:36 +0100

    fix bug bills file null

--- Billed-app-FR-Back/models/user.js
+++ Billed-app-FR-Back/models/user.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   User.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
